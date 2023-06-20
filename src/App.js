423dde21a1dbd7a67d9db28d0e6467467c24ebb5
Hafid Eledath <hafideledath@gmail.com>
2023-06-20 07:30:00 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,32 @@ import Landing from './Landing';
 import Map from './Map';
 import Itinerary from './Itinerary';
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDJ3M1axgd31Fd5Hz4rlqvzC2lWEtq7rEA",
+  authDomain: "discoverdubai-1de93.firebaseapp.com",
+  projectId: "discoverdubai-1de93",
+  storageBucket: "discoverdubai-1de93.appspot.com",
+  messagingSenderId: "59761132",
+  appId: "1:59761132:web:4b2f5081964b9c418120c9",
+  measurementId: "G-HL3K1T4G3F"
+})
+
+const auth = firebase.auth();
+
 function App() {
+  const [user] = useAuthState(auth);
+
+  return (
+    user ? ServeApp() : <SignIn />
+  );
+}
+
+function ServeApp() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
@@ -13,10 +38,19 @@ function App() {
           <Route path="/map" Component={Map} />
           <Route path="/itinerary" Component={Itinerary} />
         </Routes>
-        <a className="sign-out" href="#">Sign out</a>
+        <a className="sign-out" onClick={() => auth.signOut()}>Sign out</a>
       </div>
     </BrowserRouter>
   );
 };
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+    return <button onClick={signInWithGoogle}>Sign in with Google</button>
+}
 
 export default App;
